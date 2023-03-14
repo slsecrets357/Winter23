75a34368bc9bd3e8ba5601077f23045df21d68b6
simon <xi.yang.li@mail.mcgill.ca>
2023-03-14 00:19:17 -0400

    a

--- 211 dpm/color_sorter.py
+++ 211 dpm/color_sorter.py
@@ -15,5 +15,41 @@ def sort_color(rgbValue):
     distances = np.linalg.norm(default_colors - rgbValue, axis=1)
     index = np.argmin(distances)
     return index, color_names[index]
+
+def lane_follower(rgbValue):
+    #takes as input an rgb value and returns a steering angle
+    #used to follow a lane where left is red and right is blue
+    #if color is green, stop
+    #classify color
+    index, color_names[index] = sort_color(rgbValue)
+    
+    #if color is red, turn right
+    if index == 1:
+        return turn_right()
+    elif index == 0:
+        return turn_left()
+    elsif index == 2:
+        return stop()
+    else:
+        return go_straight()
+
+# functions returning steering rate of left and right wheels
+# to be called in state machine (not yet implemented)
+# steering rate between 0 and 1, needs to be scaled to actual speed
+def stop():
+    #stop the car
+    return 0,0
+
+def go_straight():
+    #go straight
+    return 1,1
+
+def turn_right():
+    #turn right
+    return 1,0
+
+def turn_left():
+    #turn left
+    return 0,1
     
     
