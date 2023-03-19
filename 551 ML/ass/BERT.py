42893c6be92318ff2ac1be9416c327569b5dd6e3
simon <xi.yang.li@mail.mcgill.ca>
2023-03-19 16:59:04 -0400

    a

---
+++ 551 ML/ass/BERT.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import nltk
+from nltk.tokenize import word_tokenize
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, get_linear_schedule_with_warmup
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import string
+import re
+
+train_df = pd.read_csv('train_data.csv', encoding='utf-8')
+test_df = pd.read_csv('test_data.csv', encoding='utf-8')
+
+"""
+Convert all text to lowercase to reduce the dimensionality 
+of the data and treat words in different cases as the same token.
+"""
+train_df['review'] = train_df['review'].str.lower()
+test_df['review'] = test_df['review'].str.lower()
+
+"""
+Removing HTML tags: The dataset may contain HTML tags that should be removed before analysis.
+"""
+def remove_html_tags(text):
+    return re.sub('<[^>]*>', '', text)
+train_df['review'] = train_df['review'].apply(remove_html_tags)
+test_df['review'] = test_df['review'].apply(remove_html_tags)
+
+"""
+Removing punctuation and special characters: These characters may not be relevant 
+for sentiment analysis and can be removed to simplify the text.
+"""
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+train_df['review'] = train_df['review'].apply(remove_punctuation)
+test_df['review'] = test_df['review'].apply(remove_punctuation)
+
+"""
+Tokenization: Split the text into individual words (tokens). This step is essential 
+for various text processing tasks, such as creating a bag-of-words representation or 
+input for word embeddings.
+"""
+nltk.download('punkt') # Download the punkt tokenizer
+train_df['review'] = train_df['review'].apply(word_tokenize)
+test_df['review'] = test_df['review'].apply(word_tokenize)
+
+print(train_df.describe())
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(train_df['review'], train_df['sentiment'], test_size=0.1, random_state=42)
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased') # Load the BERT tokenizer. 
+
+def encode_texts(texts, max_len=256):
+    """
+    Tokenize the texts and map the tokens to their word IDs.
+    attention_masks indicate to the model which tokens should be attended to, 
+    which is just the tokens with real content and not the padding tokens.
+    """
+    input_ids = []
+    attention_masks = []
+
+    for text in texts:
+        tokens = tokenizer.encode_plus(
+            text,
+            max_length=max_len,
+            pad_to_max_length=True,
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+        input_ids.append(tokens['input_ids'])
+        attention_masks.append(tokens['attention_mask'])
+
+    input_ids = torch.cat(input_ids, dim=0)
+    attention_masks = torch.cat(attention_masks, dim=0)
+
+    return input_ids, attention_masks
+
+train_input_ids, train_attention_masks = encode_texts(train_texts) 
+val_input_ids, val_attention_masks = encode_texts(val_texts)
+
+# Convert the labels to pytorch tensors
+train_dataset = TensorDataset(train_input_ids, train_attention_masks, torch.tensor(train_labels.values))
+val_dataset = TensorDataset(val_input_ids, val_attention_masks, torch.tensor(val_labels.values))
+print("Training dataset size: ", len(train_dataset))
+print("Validation dataset size: ", len(val_dataset))
+batch_size = 16
+
+# Create the DataLoaders for our training and validation sets.
+train_dataloader = DataLoader(train_dataset, sampler=RandomSampler(train_dataset), batch_size=batch_size)
+val_dataloader = DataLoader(val_dataset, sampler=SequentialSampler(val_dataset), batch_size=batch_size)
+print("Training dataloader size: ", len(train_dataloader))
+print("Validation dataloader size: ", len(val_dataloader))
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"device is {device}!!!")
+# Load BertForSequenceClassification, the pretrained BERT model with a single linear classification layer on top.
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+model.to(device)
+
+# AdamW is a class from the huggingface library (as opposed to pytorch). It uses weight decay instead of L2 regularization.
+optimizer = AdamW(model.parameters(), lr=2e-5, eps=1e-8) # Default parameters and optimization algorithm
+num_epochs = 3
+total_steps = len(train_dataloader) * num_epochs
+# Create the learning rate scheduler. It is used to decrease the learning rate as the training progresses.
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
+def train_epoch(model, dataloader, optimizer, scheduler, device):
+    model.train() # model is the BertForSequenceClassification model
+    for step, batch in enumerate(dataloader):
+        input_ids, attention_masks, labels = batch
+        input_ids = input_ids.to(device)
+        attention_masks = attention_masks.to(device)
+        labels = labels.to(device)
+
+        model.zero_grad() # Clear any previously calculated gradients before performing a backward pass.
+        outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
+        loss = outputs.loss
+        loss.backward()
+        print(f"Batch {step} of {len(dataloader)} loss: {loss.item()}")
+        
+        # Clip the norm of the gradients to 1.0. helps prevent the exploding gradients
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) 
+
+        optimizer.step() # Update parameters and take a step using the computed gradient.
+        scheduler.step() # Update the learning rate.
+
+def evaluate(model, dataloader, device):
+    model.eval()
+    total_eval_accuracy = 0
+
+    for batch in dataloader:
+        input_ids, attention_masks, labels = batch
+        input_ids = input_ids.to(device)
+        attention_masks = attention_masks.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=attention_masks, labels=labels)
+
+        logits = outputs.logits
+        preds = torch.argmax(logits, dim=1).cpu().numpy()
+        total_eval_accuracy += accuracy_score(labels.cpu().numpy(), preds)
+
+    avg_val_accuracy = total_eval_accuracy / len(dataloader)
+    return avg_val_accuracy
+
+# Train the model
+for epoch in range(num_epochs):
+    print(f"Epoch {epoch+1}/{num_epochs}")
+    print("-" * 10)
+
+    train_epoch(model, train_dataloader, optimizer, scheduler, device)
+    val_accuracy = evaluate(model, val_dataloader, device)
+
+    print(f"Validation accuracy: {val_accuracy:.4f}")
--- 551 ML/ass/NaiveBayes.py
+++ 551 ML/ass/NaiveBayes.py
@@ -15,6 +15,8 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import CountVectorizer
+from multiprocessing import Pool, cpu_count
+from collections import defaultdict, Counter # For word counts
 
 # Define path to data
 # data_dir = "C:/Users/SLsec/Downloads/aclImdb/"
@@ -90,6 +92,7 @@ class NaiveBayes:
         self.class_counts = None # Dictionary of class counts
         self.word_counts = None # Dictionary of word counts
         self.vocabulary = None # Set of unique words
+        self.word_counts = defaultdict(Counter)
 
     def fit(self, X, y):
         print(f"Training the model... X: {X.shape}, y: {y.shape}")
@@ -99,31 +102,65 @@ class NaiveBayes:
         self.word_counts = {c: {word: 0 for word in self.vocabulary} for c in self.classes}
 
         # Count the number of times each word appears in each class. Zip returns an iterator of tuples
-        for x, label in zip(X, y):
+        for x, label in zip(X, y): 
+            self.word_counts[label].update(x) 
             for word in x: 
                 self.word_counts[label][word] += 1
         print("done training model.")
+        
+        self.log_priors = {}
+        self.log_word_probs = defaultdict(lambda: defaultdict(float))
 
+        for c in self.classes:
+            self.log_priors[c] = np.log(self.class_counts[c] / sum(self.class_counts.values()))
+            total_words = sum(self.word_counts[c].values()) + len(self.vocabulary)
+            for word in self.vocabulary:
+                word_count = self.word_counts[c][word] + 1  # Additive (Laplace) smoothing
+                self.log_word_probs[c][word] = np.log(word_count / total_words)
+                
     def predict(self, X):
+        t1 = time.time()
         print(f"Predicting... X: {X.shape}")
         predictions = []
         i = 0
         for x in X:
             probabilities = []
-            for c in self.classes: 
-                log_prob = np.log(self.class_counts[c] / sum(self.class_counts.values())) # Prior probability
-                for word in x: 
+            x_unique = set(x)  # Only loop through unique words in the sample
+
+            for c in self.classes:
+                log_prob = self.log_priors[c]
+                for word in x_unique:
                     if word in self.vocabulary:
-                        word_count = self.word_counts[c][word] + 1  # Additive (Laplace) smoothing
-                        total_words = sum(self.word_counts[c].values()) + len(self.vocabulary)
-                        log_prob += np.log(word_count / total_words)
+                        log_prob += x.count(word) * self.log_word_probs[c][word]
                 probabilities.append(log_prob)
+
             predictions.append(self.classes[np.argmax(probabilities)])
             i+=1
             if i%20 == 0:
-                print(str(i)+") probabilities: ", probabilities)
+                print(str(i)+") probabilities: ", probabilities, "time: ", time.time()-t1)
         return predictions
-
+    # def predict(self, X):
+    #     t1 = time.time()
+    #     print(f"Predicting... X: {X.shape}")
+    #     predictions = []
+    #     i = 0
+    #     for x in X:
+    #         probabilities = []
+    #         x_unique = set(x)  # Only loop through unique words in the sample
+    #         for c in self.classes: 
+    #             log_prob = np.log(self.class_counts[c] / sum(self.class_counts.values())) # Prior probability
+    #             for word in x: 
+    #                 if word in self.vocabulary:
+    #                     word_count = self.word_counts[c][word] + 1  # Additive (Laplace) smoothing
+    #                     total_words = sum(self.word_counts[c].values()) + len(self.vocabulary)
+    #                     log_prob += np.log(word_count / total_words)
+    #             probabilities.append(log_prob)
+    #         predictions.append(self.classes[np.argmax(probabilities)])
+    #         i+=1
+    #         if i%20 == 0:
+    #             print(str(i)+") probabilities: ", probabilities, "time: ", time.time()-t1)
+    #     return predictions
+    
     def evaluate_acc(self, y_true, y_pred):
         correct = sum(y_true == y_pred)
         total = len(y_true)
@@ -139,8 +176,10 @@ nb_classifier = NaiveBayes()
 nb_classifier.fit(X_train, y_train)
 
 # Make predictions
-num = 357
+num = 100
+t1 = time.time()
 y_pred = nb_classifier.predict(X_test[:num])
+print("prediction time: ", time.time()-t1)
 
 # Evaluate the model
 accuracy = nb_classifier.evaluate_acc(y_test[:num], y_pred)
