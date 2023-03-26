00ce97aeddf8e2866744005b6267a56d97500d98
simon <xi.yang.li@mail.mcgill.ca>
2023-03-26 19:08:07 -0400

    a

--- python/test.py
+++ python/test.py
@@ -1,14 +1,30 @@
 import numpy as alex
 import time
 import math
-
-a = alex.random.randint(low=0, high=256, size=(640,480,3))
-shape = a.shape
+table1 = alex.zeros(1500)
+table2 = alex.zeros(1500)
+for i in range(1500):
+    table1[i] = i/1000
+    table2[i] = math.exp(3.57*0.001*i-4.3)
 t1 = time.time()
-for i in range(10000):
-    a.flatten()
-    a = a.reshape(shape).astype(alex.uint8)
-print("time: ", time.time() -t1)
+for j in range(1000):
+    for i in table1:
+        i2 = table2[int(i*1000)]
+print(time.time()-t1)
+
+t2 = time.time()
+for j in range(1000):
+    for i in table1:
+        i2 = math.exp(3.57*i-4.3)
+print(time.time()-t2)
+    
+# a = alex.random.randint(low=0, high=256, size=(640,480,3))
+# shape = a.shape
+# t1 = time.time()
+# for i in range(10000):
+#     a.flatten()
+#     a = a.reshape(shape).astype(alex.uint8)
+# print("time: ", time.time() -t1)
     
 # theta = 2.3562
 # theta2 = -2.3562
